6ce1b2d63e39699ff4aece34a7b9e72440a91a5b
Mario Petkov <mario.18@abv.bg>
2023-02-23 18:23:30 +0200

    all

--- Algorithms/Dynamic Programming Algorithms/0-1Knapsack Problem code.cs
+++ Algorithms/Dynamic Programming Algorithms/0-1Knapsack Problem code.cs
@@ -1,68 +1,104 @@
-namespace ConsoleApp17
+namespace AlgorithmsNetCore6
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
+    using System.Collections.Generic;
+
     public class Program
     {
-        static void Main(string[] args)
+        private static List<Item> items = new List<Item>();
+        static void Main()
         {
-            var n = int.Parse(Console.ReadLine());
+            var maxCapacity = int.Parse(Console.ReadLine());
 
-            var items = new List<Item>();
+            var line = string.Empty;
 
-            for (int i = 0; i < n; i++)
+
+            while ((line = Console.ReadLine()) != "end")
             {
-                var input = Console.ReadLine().Split();
+                var args = line.Split();
 
-                items.Add(new Item(input[0], int.Parse(input[1]), int.Parse(input[2])));
+                var name = args[0];
+                var weight = int.Parse(args[1]);
+                var value = int.Parse(args[2]);
+
+                items.Add(new Item(name, weight, value));
             }
 
-            var bagCapacity = int.Parse(Console.ReadLine());
+            var dp = new int[items.Count + 1, maxCapacity + 1];
+            var used = new bool[items.Count + 1, maxCapacity + 1];
 
-            var dp = new int[items.Count + 1, bagCapacity + 1];
-            var used = new bool[items.Count + 1, bagCapacity + 1];
-
-            for (int itemIndex = 0; itemIndex < items.Count; itemIndex++)
+            for (int row = 1; row < dp.GetLength(0); row++)
             {
-                var rowIndex = itemIndex + 1;
+                var itemIndex = row - 1;
                 var item = items[itemIndex];
 
-                for (int capacity = 0; capacity <= bagCapacity; capacity++)
+                for (int capacity = 1; capacity < dp.GetLength(1); capacity++)
                 {
+                    var excluding = dp[row - 1, capacity];
+
                     if (item.Weight > capacity)
                     {
+                        dp[row, capacity] = excluding;
                         continue;
                     }
 
-                    var notAdd = dp[rowIndex - 1, capacity];
-                    var add = item.Value + dp[rowIndex - 1, capacity - item.Weight];
+                    var including = dp[row - 1, capacity - item.Weight] + item.Value;
 
-                    if (add > notAdd)
+                    if (including > excluding)
                     {
-                        dp[rowIndex, capacity] = add;
-                        used[rowIndex, capacity] = true;
+                        dp[row, capacity] = including;
+                        used[row, capacity] = true;
                     }
                     else
                     {
-                        dp[rowIndex, capacity] = notAdd;
+                        dp[row, capacity] = excluding;
                     }
                 }
             }
 
-            Console.WriteLine(dp[items.Count, bagCapacity]);
-        }
-        public class Item
-        {
-            public Item(string name, int weight, int value)
+            var currentCapacity = maxCapacity;
+
+            var totalWeight = 0;
+
+            var res = new List<string>();
+
+            for (int row = dp.GetLength(0) - 1; row > 0; row--)
             {
-                Name = name;
-                Weight = weight;
-                Value = value;
+                if (!used[row, currentCapacity])
+                {
+                    continue;
+                }
+
+                var item = items[row - 1];
+                currentCapacity -= item.Weight;
+                totalWeight += item.Weight;
+
+                res.Add(item.Name);
+
+                if (currentCapacity == 0)
+                {
+                    break;
+                }
             }
-            public string Name { get; set; }
-            public int Weight { get; set; }
-            public int Value { get; set; }
+
+            Console.WriteLine($"Total Weight: {totalWeight}");
+            Console.WriteLine($"Total Value: {dp[items.Count, maxCapacity]}");
+            Console.WriteLine(string.Join("\n", res.OrderBy(x => x)));
         }
+    }
+
+    public class Item
+    {
+        public Item(string name, int weight, int value)
+        {
+            Name = name;
+            Weight = weight;
+            Value = value;
+        }
+
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Value { get; set; }
     }
 }
